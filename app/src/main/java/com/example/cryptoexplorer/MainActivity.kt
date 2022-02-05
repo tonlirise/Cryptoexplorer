55a11f60f0543aedc0d55d733fbef23dcdf84cf3
tonlirise <tonlirise@gmail.com>
2022-02-06 01:36:48 +0700

    Добавлен механизм получение данных от api

--- app/src/main/java/com/example/cryptoexplorer/MainActivity.kt
+++ app/src/main/java/com/example/cryptoexplorer/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.cryptoexplorer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.cryptoexplorer.api.ApiFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val disposable = ApiFactory.apiService.getTopCoinInfo()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                       Log.d("TEST_OF_LOADING_DATA", it.toString())
+            },{
+                Log.d("TEST_OF_LOADING_DATA", it?.message?:"no message")
+            })
+        compositeDisposable.add(disposable)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
---
+++ app/src/main/java/com/example/cryptoexplorer/api/ApiService.kt
@@ -0,0 +1,37 @@
+package com.example.cryptoexplorer.api
+
+import com.example.cryptoexplorer.pojo.CoinInfoListOfData
+import com.example.cryptoexplorer.pojo.CoinPriceInfoRawData
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("top/totalvolfull")
+    fun getTopCoinInfo(
+        @Query(QUERY_PARAM_API_KEY) api_key : String = API_KEY,
+        @Query(QUERY_PARAM_LIMIT) limit : Int = 10,
+        @Query(QUERY_PARAM_TSYM) tsym : String = CURRENCY
+    ): Single<CoinInfoListOfData>
+
+    @GET("pricemultifull")
+    fun getFullPriceList(
+        @Query(QUERY_PARAM_API_KEY) api_key : String = API_KEY,
+        @Query(QUERY_PARAM_FSYMS) fsyms : String,
+        @Query(QUERY_PARAM_TSYMS) tsyms : String = CURRENCY
+    ) : Single<CoinPriceInfoRawData>
+
+    companion object {
+        private const val QUERY_PARAM_API_KEY = "api_key"
+
+        private const val QUERY_PARAM_LIMIT = "limit"
+        private const val QUERY_PARAM_TSYM = "tsym"
+
+        private const val QUERY_PARAM_FSYMS = "fsyms"
+        private const val QUERY_PARAM_TSYMS = "tsyms"
+
+        private const val API_KEY = "fd17592732758118c86b6258712661ea929c361541ab1ddb2641ed3ce7b95c47"
+        private const val CURRENCY = "USD"
+    }
+}
